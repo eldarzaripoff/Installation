a3cd4580367bff49306bada1f3c9de027865433a
Eldar JDPTS-33 <eldar.zaripoff@gmail.com>
2023-02-20 15:36:11 +0300

    Second commit

---
+++ src/Creator.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Creator {
+
+    IOException e = new IOException("Fault!");
+
+    StringBuilder stringBuilder = new StringBuilder();
+    public File folderCreator(String path) {
+        File file = new File(path);
+        file.mkdir();
+        return file;
+    }
+
+    public File fileCreator (String path) {
+        File file = new File("C://Users//Эльдар//Games//src//main//Utils.java");
+        try{
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return file;
+    }
+    public StringBuilder createRecorder(File file) throws IOException {
+        if(file.mkdir()) {
+            stringBuilder.append("Folder/File ")
+                    .append(file)
+                    .append(" is created")
+                    .append('\n');
+
+        }
+        else {
+            stringBuilder.append(e.getMessage())
+                    .append('\n')
+                    .append("File wasn't created")
+                    .append('\n');
+        }
+        return stringBuilder;
+
+    }
+
+    public void recordWriter() {
+        try(FileWriter writer = new FileWriter("C://Users//Эльдар//Games//temp//temp.txt")) {
+            writer.write(stringBuilder.toString());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
